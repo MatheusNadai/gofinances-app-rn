f0397405b7f3fbca88f32adab5ecf32141c2c56b
Matheus de Nadai <matheus.nadai@hotmail.com>
2022-01-01 23:41:59 -0300

    Create header and add responsive lib

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { View, Text } from "react-native";
-import { Container, Title } from "./styles";
+import { Container, Header, Photo, User, UserGretting, UserInfo, UserName, UserWrapper } from "./styles";
 
 export function Dashboard(){
     return(
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+                    <UserInfo>
+                        <Photo
+                        source={{
+                            uri: "https://github.com/MatheusNadai.png",
+                        }}
+                        />
+                        <User>
+                            <UserGretting>Olá,</UserGretting>
+                            <UserName>Matheus</UserName>
+                        </User>
+                    </UserInfo>
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
