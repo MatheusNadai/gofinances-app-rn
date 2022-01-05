58d6a7136b05d24a20d76cfae49766eab733dabc
Matheus de Nadai <matheus.nadai@hotmail.com>
2022-01-05 17:23:37 -0300

    Add async storage

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
 import { Button } from "../../components/Form/Button";
-import { Alert, AsyncStorage, Keyboard, Modal, TouchableWithoutFeedback } from "react-native";
+import { Alert, Keyboard, Modal, TouchableWithoutFeedback } from "react-native";
 import * as Yup from 'yup'
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
 import { CategorySelectButton } from "../../components/Form/CategorySelectButton";
 import { Input } from "../../components/Form/Input";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { InputForm } from "../../components/Form/InputForm";
 import { TransactionTypeButton } from "../../components/Form/TransactionTypeButton";
 import { Container, Fields, Form, Header, Title, TransactionsType } from './styles'
@@ -67,14 +68,40 @@ export function Register() {
           return Alert.alert('Selecione a categoria');
     
     
-        const data = {
+        const newTransaction = {
           name: form.name,
           amount: form.amount,
           type: transactionType,
           category: category.key,
         }
-    
-        console.log(data)
+
+        try {
+            const dataKey = '@gofinances:transactions';
+      
+            const data = await AsyncStorage.getItem(dataKey);
+            const currentData = data ? JSON.parse(data) : [];
+      
+            const dataFormatted = [
+              ...currentData,
+              newTransaction
+            ];
+      
+            await AsyncStorage.setItem(dataKey, JSON.stringify(dataFormatted));
+      
+            reset();
+            setTransactionType('');
+            setCategory({
+              key: 'category',
+              name: 'Categoria'
+            });
+      
+            navigation.navigate('Listagem');
+            
+          } catch (error) {
+            console.log(error);
+            Alert.alert("Não foi possível salvar");
+          }
+
     }
         
     
